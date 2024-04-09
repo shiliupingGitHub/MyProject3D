350922a571e20006cd90cb4a5ab40f7e6985f45a
shiliupingGitHub <582441842@qq.com>
2024-04-09 14:23:25 +0800

    23

--- Assets/Game/Script/Map/Actor/PlayerStartActor.cs
+++ Assets/Game/Script/Map/Actor/PlayerStartActor.cs
@@ -8,22 +8,5 @@ namespace Game.Script.Map.Actor
 {
     public class PlayerStartActor : MapActor
     {
-        public GameObject Monster;
-        
-        protected override void Start()
-        {
-            base.Start();
-
-            if (Common.Game.Instance.Mode == GameMode.Host)
-            {
-                var position = transform.position;
-                position.x += 0.5f;
-                position.z += 0.5f;
-                position.y += 1;
-                var go = Instantiate(Monster, position, Quaternion.identity);
-                NetworkServer.Spawn(go);
-            }
-            
-        }
     }
 }
