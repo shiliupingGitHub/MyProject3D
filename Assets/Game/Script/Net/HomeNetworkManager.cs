8e2ea6b4002fcd1cfa5295ebaf959f3440d8fe76
shiliupingGitHub <582441842@qq.com>
2024-04-12 15:30:44 +0800

    112

---
+++ Assets/Game/Script/Net/HomeNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using Game.Script.Subsystem;
+using Mirror;
+
+namespace Game.Script.Misc
+{
+    public class HomeNetworkManager : NetworkManager
+    {
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+
+            var player = Common.Game.Instance.serverFightNewPlayer.Invoke();
+            
+            player.name = $"{player.name} [connId={conn.connectionId}]";
+            NetworkServer.AddPlayerForConnection(conn, player);
+        }
+
+        public override void OnServerSceneChanged(string sceneName)
+        {
+            base.OnServerSceneChanged(sceneName);
+
+            if (sceneName == onlineScene)
+            {
+                var eventSubsystem = Common.Game.Instance.GetSubsystem<EventSubsystem>();
+                eventSubsystem.Raise("serverSceneChanged");
+            }
+        }
+
+        public override void OnClientDisconnect()
+        {
+            base.OnClientDisconnect();
+            var eventSubsystem = Common.Game.Instance.GetSubsystem<EventSubsystem>();
+            eventSubsystem.Raise("LeaveMap");
+            var levelSystem = Common.Game.Instance.GetSubsystem<LevelSubsystem>();
+            levelSystem.Enter(LevelType.Hall);
+            
+        }
+    }
+}
