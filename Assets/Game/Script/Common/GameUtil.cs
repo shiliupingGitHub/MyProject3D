75cb88fe3da598ca2c287c4525f9a9a883ce231a
shiliupingGitHub <582441842@qq.com>
2024-04-12 16:42:40 +0800

    121

--- Assets/Game/Script/Common/GameUtil.cs
+++ Assets/Game/Script/Common/GameUtil.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Game.Script.Common
 {
@@ -16,6 +17,53 @@ namespace Game.Script.Common
             
             
             return ret;
+        }
+        public static object ConvertStringToType(string value, Type targetType)
+        {
+            // 对于值类型，需要考虑转换失败的情况
+            if (targetType.IsValueType)
+            {
+                // 尝试将字符串转换为目标类型
+                try
+                {
+                    // 对于整数类型
+                    if (targetType == typeof(int))
+                    {
+                        return Convert.ToInt32(value);
+                    }
+
+                    // 对于浮点数类型
+                    if (targetType == typeof(double))
+                    {
+                        return Convert.ToDouble(value);
+                    }
+
+                    // 对于布尔类型
+                    if (targetType == typeof(bool))
+                    {
+                        return Convert.ToBoolean(value);
+                    }
+
+                    if (targetType == typeof(string))
+                    {
+                        return value;
+                    }
+
+                    if (targetType == typeof(float))
+                    {
+                        return Convert.ToSingle(value);
+                    }
+                    // 对于其他值类型可以继续添加
+                }
+                catch
+                {
+                    // 转换失败，可以抛出异常或返回默认值
+                    return Activator.CreateInstance(targetType);
+                }
+            }
+
+            // 对于引用类型，直接转换（通常不会成功，因为会转换为对应的字符串）
+            return Convert.ChangeType(value, targetType);
         }
     }
 }
--- Assets/Game/Script/Subsystem/GameCheatSubsystem.cs
+++ Assets/Game/Script/Subsystem/GameCheatSubsystem.cs
@@ -66,7 +66,7 @@ namespace Game.Script.Subsystem
             List<object> parameterObjects = new();
             foreach (var parameter in parameters)
             {
-                var arg = ConvertStringToType(args[index], parameter.ParameterType);
+                var arg = GameUtil.ConvertStringToType(args[index], parameter.ParameterType);
                 parameterObjects.Add(arg);
                 index++;
             }
@@ -93,55 +93,7 @@ namespace Game.Script.Subsystem
 
             return ret;
         }
-
-        public static object ConvertStringToType(string value, Type targetType)
-        {
-            // 对于值类型，需要考虑转换失败的情况
-            if (targetType.IsValueType)
-            {
-                // 尝试将字符串转换为目标类型
-                try
-                {
-                    // 对于整数类型
-                    if (targetType == typeof(int))
-                    {
-                        return Convert.ToInt32(value);
-                    }
-
-                    // 对于浮点数类型
-                    if (targetType == typeof(double))
-                    {
-                        return Convert.ToDouble(value);
-                    }
-
-                    // 对于布尔类型
-                    if (targetType == typeof(bool))
-                    {
-                        return Convert.ToBoolean(value);
-                    }
-
-                    if (targetType == typeof(string))
-                    {
-                        return value;
-                    }
-
-                    if (targetType == typeof(float))
-                    {
-                        return Convert.ToSingle(value);
-                    }
-                    // 对于其他值类型可以继续添加
-                }
-                catch
-                {
-                    // 转换失败，可以抛出异常或返回默认值
-                    return Activator.CreateInstance(targetType);
-                }
-            }
-
-            // 对于引用类型，直接转换（通常不会成功，因为会转换为对应的字符串）
-            return Convert.ChangeType(value, targetType);
-        }
-
+        
         void OnUpdate(float deltaTime)
         {
             if (!UIManager.Instance.IsInit)
